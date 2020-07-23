183c481555e6d0b4bd249b5e1a5ad37505b5d0d2
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-23 16:01:09 +0900

    Use dotenv Module to Load Config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { PORT } from "./config";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000.");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
 });
