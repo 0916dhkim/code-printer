70d4ca9a21be3026e5a74bc18367fbf5a7cb0b4b
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-23 20:39:58 +0900

    Add URL Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [urlInput, setUrlInput] = useState("");
+  function renderCode(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    throw new Error("Not implemented.");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Code Printer</h1>
+        <form onSubmit={renderCode}>
+          <label>
+            URL
+            <input type="url" name="url" value={urlInput} onChange={e => setUrlInput(e.target.value)} />
+          </label>
+          <input type="submit" value="Render" />
+        </form>
       </header>
     </div>
   );
