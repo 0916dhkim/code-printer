5628e81c3504f6a884150c8a0538ac3d7b340bb2
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-24 17:03:25 +0900

    Update Document Title After Loading Code

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -4,39 +4,41 @@ import { ApplicationState, ApplicationDispatch } from "./store";
 import axios from "axios";
 
 /**
- * Convert GitHub file URL to raw data URL.
- * @param githubUrl URL of file on GitHub.
- */
-async function githubUrlToRaw(githubUrl: string): Promise<string> {
-  const parsed = new URL(githubUrl);
-  const pathArray = parsed.pathname.split("/");
-  if (parsed.host !== "github.com" || pathArray[3] !== "blob") {
-    throw new Error("Invalid GitHub URL.");
-  }
-  parsed.host = "raw.githubusercontent.com";
-  parsed.pathname = [
-    ...pathArray.slice(0, 3),
-    ...pathArray.slice(4)
-  ].join("/");
-  return parsed.toString();
-}
-
-/**
  * React Component for Getting User Input.
  */
 export default function Controls() {
   const dispatch = useDispatch<ApplicationDispatch>();
   const urlInput = useSelector((state: ApplicationState) => state.urlInput);
   const languageInput = useSelector((state: ApplicationState) => state.languageInput);
-  function renderCode(e: React.FormEvent<HTMLFormElement>) {
+
+  async function renderCode(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     dispatch({ type: "START_LOADING" });
-    githubUrlToRaw(urlInput)
-      .then(rawUrl => axios.get(rawUrl))
-      .then(res => {
-        dispatch({ type: "LOAD_SOURCE_CODE", value: res.data });
-      })
-      .catch(e => {
+
+    try {
+      // Parse URL input.
+      const parsed = new URL(urlInput);
+      const pathArray = parsed.pathname.split("/");
+      if (parsed.host !== "github.com" || pathArray[3] !== "blob") {
+        throw new Error("Invalid GitHub URL.");
+      }
+
+      // Convert GitHub URL to GitHub raw file URL.
+      parsed.host = "raw.githubusercontent.com";
+      parsed.pathname = [
+        ...pathArray.slice(0, 3),
+        ...pathArray.slice(4)
+      ].join("/");
+
+      // Get raw content.
+      const fetchResult = await axios.get(parsed.toString());
+
+      // Update page title.
+      document.title = pathArray.slice(4).join("/");
+
+      // Update application state.
+      dispatch({ type: "LOAD_SOURCE_CODE", value: fetchResult.data });
+    } catch (e) {
         let error: Error;
         if (typeof e === "string") {
           error = new Error(e);
@@ -47,7 +49,7 @@ export default function Controls() {
         }
 
         dispatch({ type: "FAIL_LOADING", error });
-      });
+    }
   }
 
   return (
