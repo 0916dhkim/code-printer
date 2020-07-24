ed54dd983f5c7575d5924767792773837dcd2390
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-24 11:36:05 +0900

    Load Source Code From GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { ApplicationState, ApplicationDispatch } from "./store";
+import axios from "axios";
+
+/**
+ * Convert GitHub file URL to raw data URL.
+ * @param githubUrl URL of file on GitHub.
+ */
+async function githubUrlToRaw(githubUrl: string): Promise<string> {
+  const parsed = new URL(githubUrl);
+  const pathArray = parsed.pathname.split("/");
+  if (parsed.host !== "github.com" || pathArray[3] !== "blob") {
+    throw new Error("Invalid GitHub URL.");
+  }
+  parsed.host = "raw.githubusercontent.com";
+  parsed.pathname = [
+    ...pathArray.slice(0, 3),
+    ...pathArray.slice(4)
+  ].join("/");
+  return parsed.toString();
+}
 
 function App() {
   const dispatch = useDispatch<ApplicationDispatch>();
   const urlInput = useSelector((state: ApplicationState) => state.urlInput);
+  const isLoading = useSelector((state: ApplicationState) => state.isLoading);
+  const sourceCode = useSelector((state: ApplicationState) => state.sourceCode);
+  const error = useSelector((state: ApplicationState) => state.error);
 
   function renderCode(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    throw new Error("Not implemented.");
+    dispatch({ type: "START_LOADING" });
+    githubUrlToRaw(urlInput)
+      .then(rawUrl => axios.get(rawUrl))
+      .then(res => {
+        dispatch({ type: "LOAD_SOURCE_CODE", value: res.data });
+      })
+      .catch(e => {
+        let error: Error;
+        if (typeof e === "string") {
+          error = new Error(e);
+        } else if (!(e instanceof Error)) {
+          error = new Error("Failed to Load Source Code.");
+        } else {
+          error = e;
+        }
+
+        dispatch({ type: "FAIL_LOADING", error });
+      });
   }
   return (
     <div className="App">
@@ -22,6 +61,9 @@ function App() {
           <input type="submit" value="Render" />
         </form>
       </header>
+      {isLoading && <p>Loading ...</p>}
+      {error && <p>{error.message}</p>}
+      {sourceCode ?? <p>{sourceCode}</p>}
     </div>
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -2,15 +2,30 @@ import { createStore } from "redux";
 
 export type ApplicationState  = {
   urlInput: string
+  isLoading: boolean
+  sourceCode?: string
+  error?: Error
 }
 
 type Action = {
   type: "SET_URL_INPUT",
   value: string
 }
+| {
+  type: "START_LOADING"
+}
+| {
+  type: "LOAD_SOURCE_CODE"
+  value: string
+}
+| {
+  type: "FAIL_LOADING",
+  error: Error
+}
 
 const initialState: ApplicationState = {
-  urlInput: ""
+  urlInput: "",
+  isLoading: false
 }
 
 function reducer(state: ApplicationState = initialState, action: Action): ApplicationState {
@@ -18,7 +33,26 @@ function reducer(state: ApplicationState = initialState, action: Action): Applic
   switch (action.type) {
     case "SET_URL_INPUT":
       return {
+        ...state,
         urlInput: action.value
+      };
+    case "START_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+        error: undefined
+      };
+    case "LOAD_SOURCE_CODE":
+      return {
+        ...state,
+        isLoading: false,
+        sourceCode: action.value
+      };
+    case "FAIL_LOADING":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error
       };
     default:
       return state;
