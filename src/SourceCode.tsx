a45731e84f2fd3d2466ed56eb76fd24e9fd59f7a
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-24 15:03:04 +0900

    Enable Syntax Highlighting

--- src/SourceCode.tsx
+++ src/SourceCode.tsx
@@ -1,4 +1,7 @@
 import React, { useMemo } from "react";
+import { highlight } from "highlight.js";
+import ReactHtmlParser from "react-html-parser";
+import "highlight.js/styles/default.css";
 
 type Props = {
   sourceCode: string
@@ -9,16 +12,27 @@ type Props = {
  */
 export function SourceCode({ sourceCode }: Props) {
   const sourceCodeLines = useMemo(() => {
-    return sourceCode.split("\n");
+    const lines = sourceCode.split("\n");
+    const ret: string[] = [];
+    let highlightState: Mode | undefined;
+    let highlightResult: HighlightResult | undefined;
+    for (let line of lines) {
+      highlightResult = highlight("ts", line, true, highlightState);
+      highlightState = highlightResult.top;
+      ret.push(highlightResult.value);
+    }
+    return ret;
   }, [sourceCode]);
   return (
     <table>
-      {sourceCodeLines.map((line, i) => (
-        <tr key={i}>
-          <td>{i + 1}</td>
-          <tr>{line}</tr>
-        </tr>
-      ))}
+      <tbody>
+        {sourceCodeLines.map((line, i) => (
+          <tr key={i}>
+            <td>{i + 1}</td>
+            <tr>{ReactHtmlParser(line)}</tr>
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
